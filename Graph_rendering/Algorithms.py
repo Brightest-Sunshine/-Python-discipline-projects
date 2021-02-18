b69712d0999aaf0f33a7bb7c9a845ad4581459ce
Dmitroved <dmitroved@gmail.com>
2021-02-19 00:14:59 +0300

    fixed BFS vis, now using different colours in BFS

--- Graph_rendering/Algorithms.py
+++ Graph_rendering/Algorithms.py
@@ -3,16 +3,16 @@ import collections
 import time
 
 NODE_COLOR = "red"
+NODE_COLORS = ["red", "blue", "green", "orange", "grey", "pink"]  # enough?
 
 
 def DFS(graph: Graph, visited, node, counter=0):  # done i guess
     if node not in visited:
         counter += 1
-        file_name = 'DFS_step_' + str(counter) + '.gv'
+        file_name = 'DFS_result\DFS_step_' + str(counter) + '.gv'
         graph.graphviz_graph.node(str(node), fillcolor=NODE_COLOR, style="filled")
-        graph.graphviz_graph.render(filename=file_name, view=True)
+        graph.graphviz_graph.render(filename=file_name, view=False)
         visited.add(node)
-        print(node)
         for neighbour in graph.adjacency_list[node]:
             counter = DFS(graph, visited, neighbour, counter)
     return counter
@@ -21,14 +21,25 @@ def DFS(graph: Graph, visited, node, counter=0):  # done i guess
 def BFS(graph: Graph, visited, node):
     queue = collections.deque([node])
     visited.add(node)
-    graph.graph.node(str(node), fillcolor=NODE_COLOR)
-    graph.draw_graph()
+    counter = 1
+    color_count = 0
+    file_name = 'BFS_result\BFS_step_' + str(counter) + '.gv'
+    graph.graphviz_graph.node(str(node), fillcolor=NODE_COLOR, style="filled")
+    graph.graphviz_graph.render(filename=file_name, view=False)
+
+    # graph.()
     while queue:
         vertex = queue.popleft()
-        for neighbour in graph[vertex]:
+        print(vertex)
+        color_count += 1
+        for neighbour in graph.adjacency_list[vertex]:
+
             if neighbour not in visited:
+                counter += 1
+                file_name = 'BFS_result\BFS_step_' + str(counter) + '.gv'
                 visited.add(neighbour)
-                graph.graph.node(str(node), fillcolor=NODE_COLOR)
-                graph.draw_graph()
+                graph.graphviz_graph.node(str(neighbour), fillcolor=NODE_COLORS[color_count], style="filled")
+                # graph.draw_graph()
+                graph.graphviz_graph.render(filename=file_name, view=False)
                 queue.append(neighbour)
     return
--- Graph_rendering/main.py
+++ Graph_rendering/main.py
@@ -1,11 +1,9 @@
 from Graph_rendering import Graph
 from Graph_rendering import Algorithms
 
-
 if __name__ == '__main__':
     graph = Graph.Graph()
     graph.draw_graph()
     visited = set()
-    Algorithms.DFS(graph, visited, 0)
-
-
+    # Algorithms.DFS(graph, visited, 0)
+    Algorithms.BFS(graph, visited, 0)
