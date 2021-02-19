0b0c2847a7107a0ca7617c4444aa1a7a700ed85d
Dmitroved <dmitroved@gmail.com>
2021-02-19 12:39:07 +0300

    create unittest, add feature for test in methods

--- Graph_rendering/Algorithms.py
+++ Graph_rendering/Algorithms.py
@@ -6,25 +6,30 @@ NODE_COLOR = "red"
 NODE_COLORS = ["red", "blue", "green", "orange", "grey", "pink"]  # enough?
 
 
-def DFS(graph: Graph, visited, node, counter=0):  # done i guess
+def DFS(graph: Graph, visited, node, counter=0, test_running=0):  # done i guess
     if node not in visited:
         counter += 1
-        file_name = 'DFS_result\DFS_step_' + str(counter) + '.gv'
-        graph.graphviz_graph.node(str(node), fillcolor=NODE_COLOR, style="filled")
-        graph.draw_graph(file_name=file_name, view=False, cleanup=True)
+        if test_running == 0:
+            file_name = 'DFS_result\DFS_step_' + str(counter) + '.gv'
+            graph.graphviz_graph.node(str(node), fillcolor=NODE_COLOR, style="filled")
+            graph.draw_graph(file_name=file_name, view=False, cleanup=True)
         visited.add(node)
         for neighbour in graph.adjacency_list[node]:
-            counter = DFS(graph, visited, neighbour, counter)
+            counter = DFS(graph, visited, neighbour, counter, test_running)
     return counter
 
 
-def BFS(graph: Graph, visited, node):
+def BFS(graph: Graph, visited, node, test_running=0):
+    path = []
     visited.add(node)
+    if test_running:
+        path = [[node,0]]
     counter = 1
     color_count = 0
-    file_name = 'BFS_result\BFS_step_' + str(counter) + '.gv'
-    graph.graphviz_graph.node(str(node), fillcolor=NODE_COLORS[color_count], style="filled")
-    graph.draw_graph(file_name=file_name, view=False, cleanup=True)
+    if test_running == 0:
+        file_name = 'BFS_result\BFS_step_' + str(counter) + '.gv'
+        graph.graphviz_graph.node(str(node), fillcolor=NODE_COLORS[color_count], style="filled")
+        graph.draw_graph(file_name=file_name, view=False, cleanup=True)
     color_count = 1
     queue = collections.deque([[node, color_count]])
     while queue:
@@ -34,7 +39,12 @@ def BFS(graph: Graph, visited, node):
                 counter += 1
                 file_name = 'BFS_result\BFS_step_' + str(counter) + '.gv'
                 visited.add(neighbour)
-                graph.graphviz_graph.node(str(neighbour), fillcolor=NODE_COLORS[color_count], style="filled")
-                graph.draw_graph(file_name=file_name, view=False, cleanup=True)
+                if test_running == 0:
+                    graph.graphviz_graph.node(str(neighbour), fillcolor=NODE_COLORS[color_count], style="filled")
+                    graph.draw_graph(file_name=file_name, view=False, cleanup=True)
+                else:
+                    path.append([neighbour, color_count+1])
                 queue.append([neighbour, color_count + 1])
-    return
+    if test_running:
+        return path
+    return visited
