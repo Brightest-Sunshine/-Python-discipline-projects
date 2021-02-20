dd1a185fdb3a4c34c200b9e301aa96672a18fd6d
Mamaeva Anastasiia <60839846+Brightest-Sunshine@users.noreply.github.com>
2021-02-20 20:13:55 +0500

    removing ported code

--- Graph_rendering/main.py
+++
@@ -1,9 +0,0 @@
-from Graph_rendering import Graph
-from Graph_rendering import Algorithms
-
-if __name__ == '__main__':
-    graph = Graph.Graph()
-    graph.draw_graph()
-    visited = set()
-    #Algorithms.DFS(graph, visited, 0)  # TODO need random node in input
-    Algorithms.BFS(graph, visited, 0)
