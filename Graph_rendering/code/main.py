6c84ebc522805353387baf9e2865c40f87fe7e20
Mamaeva Anastasiia <60839846+Brightest-Sunshine@users.noreply.github.com>
2021-02-20 20:08:46 +0500

    Create main.py

---
+++ Graph_rendering/code/main.py
@@ -0,0 +1,8 @@
+from task import Algorithms, Graph
+
+if __name__ == '__main__':
+    graph = Graph.Graph()
+    graph.draw_graph()
+    visited = set()
+    # Algorithms.DFS(graph, visited, 0)
+    Algorithms.BFS(graph, visited, 0)
